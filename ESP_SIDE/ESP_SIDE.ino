58a9173dbb6d86f80b65c57b01f54f940b5fed35
ivanderjeconiah <78361298+ivanderjeconiah@users.noreply.github.com>
2023-04-22 23:25:06 +0700

    Update ESP_SIDE.ino

--- ESP_SIDE/ESP_SIDE.ino
+++ ESP_SIDE/ESP_SIDE.ino
@@ -10,47 +10,55 @@
 //variable declaration
 char ssid[] = "YourNetworkName";
 char pass[] = "YourPassword";
-String receivedData,bantu;
+String receivedData, bantu;
 int j;
-float data[8];
+float data[9];
 
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 
-void loop(){
+void loop() {
   Blynk.run();
-  if (Serial.available()){
-    receivedData= Serial.readStringUntil('=');
-    j=0;
-    bantu="";
-    for (int i=0; i< receivedData.length(); i++){
-      if(receivedData[i] == ' '){
-        data[j]=  bantu.toFloat();
-        bantu="";
+  if (Serial.available()) {
+    receivedData = Serial.readStringUntil('=');
+    j = 0;
+    bantu = "";
+    for (int i = 0; i < receivedData.length(); i++) {
+      if (receivedData[i] == ' ') {
+        data[j] =  bantu.toFloat();
+        bantu = "";
         j++;
       }
-      else{
-        if((isDigit(receivedData[i])) || (receivedData[i]=='.')){
-          bantu=bantu+receivedData[i];
+      else {
+        if ((isDigit(receivedData[i])) || (receivedData[i] == '.')) {
+          bantu = bantu + receivedData[i];
         }
-        else{
-          bantu="";
+        else {
+          bantu = "";
         }
       }
     }
-    data[j]= bantu.toFloat();n
+    data[j] = bantu.toFloat();
+    Blynk.virtualWrite(V0, data[0]);
+    Blynk.virtualWrite(V1, data[1]);
+    Blynk.virtualWrite(V2, data[2]);
+    Blynk.virtualWrite(V3, data[3]);
+    Blynk.virtualWrite(V4, data[4]);
+    Blynk.virtualWrite(V5, data[5]);
+    Blynk.virtualWrite(V6, data[6]);
+    Blynk.virtualWrite(V7, data[7]);
+    Blynk.virtualWrite(V8, data[8]);
   }
+  delay(50);
+}
 
-  Blynk.virtualWrite(V0,data[0]);
-  Blynk.virtualWrite(V1,data[1]);
-  Blynk.virtualWrite(V2,data[2]);
-  Blynk.virtualWrite(V3,data[3]);
-  Blynk.virtualWrite(V4,data[4]);
-  Blynk.virtualWrite(V5,data[5]);
-  Blynk.virtualWrite(V6,data[6]);
-  Blynk.virtualWrite(V7,data[7]);
+BLYNK_WRITE(V9){
+  bool hasil = param.asInt();
+  if(hasil){
+     Serial.write("1=");
+  }
 }
